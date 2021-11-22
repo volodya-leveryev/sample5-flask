fce233acb239fc74b5391bda1dc386f20aa1ea7f
Volodya Leveryev <volodya.leveryev@gmail.com>
2021-11-22 15:48:19 +0900

    Add create, update, delete feature

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
+csrf = CSRFProtect(app)
 
 
 class Student(db.Model):
@@ -17,15 +20,34 @@ class Student(db.Model):
     number = db.Column(db.String(20), unique=True, nullable=False)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index_page():
+    students = Student.query.all()
+    return render_template('index.html', objects=students)
+
+
+@app.route("/form/", methods=['GET', 'POST'])
+@app.route("/form/<int:student_id>/", methods=['GET', 'POST'])
+def student_form(student_id=None):
+    student = Student.query.get_or_404(student_id) if student_id else None
     if request.method == 'POST':
-        student = Student()
+        if student is None:
+            student = Student()
         student.last_name = request.form['last_name']
         student.first_name = request.form['first_name']
         student.second_name = request.form['second_name']
         student.number = request.form['number']
         db.session.add(student)
         db.session.commit()
-    students = Student.query.all()
-    return render_template('index.html', objects=students)
+        return redirect('/')
+    return render_template('form.html', obj=student)
+
+
+@app.route("/delete/<int:student_id>/", methods=['GET', 'POST'])
+def student_delete(student_id):
+    student = Student.query.get_or_404(student_id)
+    if request.method == 'POST':
+        db.session.delete(student)
+        db.session.commit()
+        return redirect('/')
+    return render_template('delete.html', obj=student)
